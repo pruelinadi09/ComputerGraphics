b1ae93fe694246c78c60748cd4bc153a17f9bd2a
pruelinadi09 <pruverna@gmail.com>
2025-03-25 13:48:52 +0800

    modify and create draw functions

--- HW1/WebGL.js
+++ HW1/WebGL.js
@@ -121,6 +121,126 @@ function click(ev){ //you may want to define more arguments for this function
 function draw(){ //you may want to define more arguments for this function
     //redraw whole canvas here
     //Note: you are only allowed to same shapes of this frame by single gl.drawArrays() call
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    drawShapes(shapes.p, drawPoints);
+    drawShapes(shapes.h, drawHorizontalLines);
+    drawShapes(shapes.v, drawVerticalLines);
+    drawShapes(shapes.t, drawTriangles);
+    drawShapes(shapes.q, drawSquares);
+    drawShapes(shapes.c, drawCircles);
+}
+
+function drawShapes(shapeList, drawFunc) {
+    if (shapeList.length > 0) drawFunc(shapeList);
+}
+
+function drawPoints(points) {
+    let vertices = [];
+    let colors = [];
+    points.forEach(p => {
+        vertices.push(p.x, p.y);
+        colors.push(...p.color);
+    });
+
+    drawArrays(vertices, colors, gl.POINTS);
+}
+
+function drawHorizontalLines(lines) {
+    let vertices = [];
+    let colors = [];
+    lines.forEach(l => {
+        vertices.push(l.x1, l.y, l.x2, l.y);
+        colors.push(...l.color, ...l.color);
+    });
+
+    drawArrays(vertices, colors, gl.LINES);
+}
+
+function drawVerticalLines(lines) {
+    let vertices = [];
+    let colors = [];
+    lines.forEach(l => {
+        vertices.push(l.x, l.y1, l.x, l.y2);
+        colors.push(...l.color, ...l.color);
+    });
+
+    drawArrays(vertices, colors, gl.LINES);
+}
+
+function drawTriangles(triangles) {
+    let vertices = [];
+    let colors = [];
+    triangles.forEach(t => {
+        let size = 0.05;
+        vertices.push(
+            t.x, t.y + size,
+            t.x - size, t.y - size,
+            t.x + size, t.y - size
+        );
+        colors.push(...t.color, ...t.color, ...t.color);
+    });
+
+    drawArrays(vertices, colors, gl.TRIANGLES);
+}
+
+function drawSquares(squares) {
+    let vertices = [];
+    let colors = [];
+    squares.forEach(q => {
+        let size = 0.05;
+        vertices.push(
+            q.x - size, q.y - size,
+            q.x + size, q.y - size,
+            q.x + size, q.y + size,
+            q.x - size, q.y - size,
+            q.x + size, q.y + size,
+            q.x - size, q.y + size
+        );
+        colors.push(...q.color, ...q.color, ...q.color, ...q.color, ...q.color, ...q.color);
+    });
+
+    drawArrays(vertices, colors, gl.TRIANGLES);
+}
+
+function drawCircles(circles) {
+    let vertices = [];
+    let colors = [];
+    let numSegments = 20;
+    let angleStep = (2 * Math.PI) / numSegments;
+
+    circles.forEach(c => {
+        for (let i = 0; i < numSegments; i++) {
+            let angle1 = i * angleStep;
+            let angle2 = (i + 1) * angleStep;
+            let size = 0.05;
+
+            let x1 = c.x + Math.cos(angle1) * size;
+            let y1 = c.y + Math.sin(angle1) * size;
+            let x2 = c.x + Math.cos(angle2) * size;
+            let y2 = c.y + Math.sin(angle2) * size;
+
+            vertices.push(c.x, c.y, x1, y1, x2, y2);
+            colors.push(...c.color, ...c.color, ...c.color);
+        }
+    });
+
+    drawArrays(vertices, colors, gl.TRIANGLES);
+}
+
+function drawArrays(vertices, colors, mode) {
+    let vertexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+    gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(a_Position);
+
+    let colorBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+    gl.vertexAttribPointer(a_Color, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(a_Color);
+
+    gl.drawArrays(mode, 0, vertices.length / 2);
 }
 
 function createProgram(gl, vShaderSrc, fShaderSrc) {
